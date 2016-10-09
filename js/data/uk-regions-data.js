fb3558053495e36e3f19609af14e55be6008d38c
Tom Ewing <ewingt1979@gmail.com>
2016-10-09 17:34:30 +0100

    Finished adding data to the index page

--- js/data/uk-regions-data.js
+++ js/data/uk-regions-data.js
@@ -28,7 +28,7 @@ var ukRegionData = {
         "axis": ["2016","2017","2018","2019","2020","2021","2022","2023","2024","2025"],
     },{
         "name": "North East",
-            "bullets": [
+        "bullets": [
                     "<li>Investment Level: £42m</li>",
                     "<li>Total schemes: 21</li>",
                     "<li>Major Schemes: 11</li>",
@@ -53,34 +53,220 @@ var ukRegionData = {
         "social": [0.1 ,1.1, 1.9 ,4.1 ,6.2 ,1.1 ,-1.4, -1.2, -1.8, -1.3],
         "axis": ["2016","2017","2018","2019","2020","2021","2022","2023","2024","2025"]
 
-        },
-
-        {
+        }, {
         "name": "North West",
+        "bullets": [
+                    "<li>Investment Level: £94</li>",
+                    "<li>Total schemes: 32</li>",
+                    "<li>Major Schemes: 15</li>",
+                    "<li>Minor Schemes: 17</li>"
+                    ],
+        "detail": [
+                    "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>",
+                    "<p>Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. </p> ",
+                    "<p>Vestibulum rhoncus efficitur velit, fermentum dictum sem suscipit quis. Nunc fermentum nisl a condimentum volutpat. Maecenas ultrices tortor et arcu euismod condimentum. </p>"
+                    ],
+        "impact": [
+                    "<li>Sed dignissim lacinia nunc. Curabitur tortor.</li>",
+                    "<li>Pellentesque nibh. Aenean quam. In scelerisque sem at dolor.</li>",
+                    "<li>Maecenas mattis. Sed convallis tristique sem.</li>",
+                    "<li>Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa.</li>",
+                    "<li>Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum.</li>"
+                   ],
+        "investment": [88,12,8,6,4,0,0,0,0,0],
+        "impact-data": [0,1,1,1,5,62,54,50,52,58],
+        "economic": [1.3, 1.4, 2.0, 2.7, 1.9, 1.6 ,1.1 , 1.5, 1.3, 1.7],
+        "environmental":[1.5, 1.1, 1.3, 0.5, 1.2, -1.3, -3.3, -4.1, -2.5, -1.9],
+        "social": [2.1 ,2.4, 2.9 ,2.1 ,2.2 ,2.1 ,2.4, 2.2, 2.8, 2.0],
+        "axis": ["2016","2017","2018","2019","2020","2021","2022","2023","2024","2025"]
         },
         {
         "name": "Greater London Authority",
+            "bullets": [
+                    "<li>Investment Level: £265m</li>",
+                    "<li>Total schemes: 30</li>",
+                    "<li>Major Schemes: 19</li>",
+                    "<li>Minor Schemes: 11</li>"
+                    ],
+        "detail": [
+                    "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.</p>",
+                    "<p>Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.</p> ",
+                    "<p>Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc.</p>"
+                    ],
+        "impact": [
+                    "<li>Curabitur tortor. Pellentesque nibh.</li>",
+                    "<li>Aenean quam. In scelerisque sem at dolor. Maecenas mattis.</li>",
+                    "<li>Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor.</li>",
+                    "<li>Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. </li>"
+                   ],
+        "investment": [102,169,83,62,41,0,0,0,0,0],
+        "impact-data": [0,30,22,50,75,75,75,52,52,54],
+        "economic": [3.4, 2.7, 3.3, 3.6, 3.8, 3.3, 4.1, 3.2, 2.8, 2.4],
+        "environmental":[-1.5, -1.1, -1.3, -0.5, -1.2, -1.3, -3.3, -4.1, -2.5, -1.9],
+        "social": [0.1 ,0.4, 0.9 ,0.1 ,0.2 ,0.1 ,0.4, 0.2, 0.8, 0.1],
+        "axis": ["2016","2017","2018","2019","2020","2021","2022","2023","2024","2025"]
+
         },
         {
         "name": "West Midlands",
+        "bullets": [
+                    "<li>Investment Level: £103</li>",
+                    "<li>Total schemes: 28</li>",
+                    "<li>Major Schemes: 14</li>",
+                    "<li>Minor Schemes: 14</li>"
+                    ],
+        "detail": [
+                    "<p><Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>",
+                    "<p>Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. </p> ",
+                    "<p>Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.</p>"
+                    ],
+        "impact": [
+                    "<li>Curabitur tortor. Pellentesque nibh. Aenean quam. </li>",
+                    "<li>In scelerisque sem at dolor. Maecenas mattis.</li>",
+                    "<li>Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor.</li>",
+                    "<li>Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. </li>",
+                    "<li>Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum.</li>"
+                   ],
+        "investment": [88,83,68,44,32,12,12,12,6,6],
+        "impact-data": [0,0,0,0,0,98,98,98,98,98],
+        "economic": [-3.2, -1.8, -0.5, -0.5, 1.6, 2.5, 2.8, 3.6, 4.1, 4.5],
+        "environmental":[0.1 ,0.4, 0.9 ,0.1 ,0.2 ,0.1 ,0.4, 0.2, 0.8, 0.1],
+        "social": [-1.5, -1.1, -1.3, -0.5, -1.2, -1.3, -3.3, -4.1, -2.5, -1.9],
+        "axis": ["2016","2017","2018","2019","2020","2021","2022","2023","2024","2025"]
         },
         {
         "name": "Yorkshire and The Humber",
-        },
-        {
-        "name": "West Midlands",
+        "bullets": [
+                    "<li>Investment Level: £94</li>",
+                    "<li>Total schemes: 32</li>",
+                    "<li>Major Schemes: 15</li>",
+                    "<li>Minor Schemes: 17</li>"
+                    ],
+        "detail": [
+                    "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam blandit elit non nunc ornare rhoncus. </p>",
+                    "<p>Maecenas ut mauris bibendum, porttitor augue at, vestibulum tellus.</p> ",
+                    "<p>Phasellus risus lorem, condimentum et ultrices vel, vehicula porta purus. Mauris consequat tristique elit, ac vestibulum justo sollicitudin sed.</p>"
+                    ],
+        "impact": [
+                    "<li>Proin tincidunt nunc sit amet suscipit semper. Vivamus ornare cursus sem, et commodo sapien venenatis ut. Sed eu sollicitudin purus, non dignissim dui.</li>",
+                    "<li>ullam eleifend urna lacus, sit amet laoreet justo faucibus in. Phasellus eu nunc semper, vehicula augue sed, suscipit arcu.</li>",
+                    "<li>Donec eleifend tortor dolor, ac vehicula nisi tempus quis. Nullam neque erat, gravida sed libero quis, mattis placerat augue.</li>",
+                    "<li>tiam eu purus tempor, vulputate ante sit amet, ornare metus. Quisque nec dui quis nisi condimentum dictum.</li>",
+                    "<li>Sed maximus nisl ut molestie efficitur. Suspendisse sem massa, pulvinar ac sagittis quis, pellentesque in libero. </li>"
+                   ],
+        "investment": [109,62,32,23,23,23,0,0,0,0],
+        "impact-data": [0,30,22,50,75,75,75,52,52,54],
+        "economic": [3.4, 2.7, 3.3, 3.6, 3.8, 3.3, 4.1, 3.2, 2.8, 2.4],
+        "environmental":[5.1, 3.4, 4.0, 3.7, 2.9, 3.6 ,5.1 , 7.2, 6.1, 5.7],
+        "social": [-3.2, -1.8, -0.5, -0.5, 1.6, 2.5, 2.8, 3.6, 4.1, 4.5],
+        "axis": ["2016","2017","2018","2019","2020","2021","2022","2023","2024","2025"]
         },
         {
         "name": "South West",
+        "bullets": [
+                    "<li>Investment Level: £42m</li>",
+                    "<li>Total schemes: 21</li>",
+                    "<li>Major Schemes: 11</li>",
+                    "<li>Minor Schemes: 10</li>"
+                    ],
+        "detail": [
+                    "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>",
+                    "<p>Donec vulputate finibus molestie. Suspendisse et quam rhoncus, suscipit nunc id, facilisis lorem. Suspendisse ac imperdiet est.</p> ",
+                    "<p>Etiam semper convallis lacus, eu lobortis mi mattis vitae. Nullam vehicula elit id purus ultrices scelerisque. Quisque vel malesuada est. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>"
+                    ],
+        "impact": [
+                    "<li>Aliquam vel lacus vel sapien pharetra tristique sed iaculis tellus.</li>",
+                    "<li>Phasellus et mauris in magna pulvinar eleifend. Suspendisse eros neque, elementum ut suscipit a, tempor et turpis.</li>",
+                    "<li>Donec justo elit, lacinia sit amet turpis vel, iaculis porttitor diam. Nullam vulputate posuere pretium.</li>",
+                    "<li>Morbi a turpis ut magna consequat venenatis. Nunc rutrum tempus massa, at tristique tortor pretium ut.</li>"
+                   ],
+        "investment": [43,26,38,14,26,8,8,4,0,0],
+        "impact-data": [0,1,1,57,66,24,40,36,62,68],
+        "economic": [5.1, 3.4, 4.0, 3.7, 2.9, 3.6 ,5.1 , 7.2, 6.1, 5.7],
+        "environmental":[3.5,2.1,3.3,0.5,-0.8,-1.1,-1.3,-2.1,0.5,1.9],
+        "social": [0.1 ,1.1, 1.9 ,4.1 ,6.2 ,1.1 ,-1.4, -1.2, -1.8, -1.3],
+        "axis": ["2016","2017","2018","2019","2020","2021","2022","2023","2024","2025"]
         },
         {
         "name": "East Midlands",
+        "bullets": [
+                    "<li>Investment Level: £94</li>",
+                    "<li>Total schemes: 32</li>",
+                    "<li>Major Schemes: 15</li>",
+                    "<li>Minor Schemes: 17</li>"
+                    ],
+        "detail": [
+                    "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>",
+                    "<p>Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. </p> ",
+                    "<p>Vestibulum rhoncus efficitur velit, fermentum dictum sem suscipit quis. Nunc fermentum nisl a condimentum volutpat. Maecenas ultrices tortor et arcu euismod condimentum. </p>"
+                    ],
+        "impact": [
+                    "<li>Sed dignissim lacinia nunc. Curabitur tortor.</li>",
+                    "<li>Pellentesque nibh. Aenean quam. In scelerisque sem at dolor.</li>",
+                    "<li>Maecenas mattis. Sed convallis tristique sem.</li>",
+                    "<li>Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa.</li>",
+                    "<li>Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum.</li>"
+                   ],
+        "investment": [88,12,8,6,4,0,0,0,0,0],
+        "impact-data": [0,1,1,1,5,62,54,50,52,58],
+        "economic": [1.3, 1.4, 2.0, 2.7, 1.9, 1.6 ,1.1 , 1.5, 1.3, 1.7],
+        "environmental":[1.5, 1.1, 1.3, 0.5, 1.2, -1.3, -3.3, -4.1, -2.5, -1.9],
+        "social": [2.1 ,2.4, 2.9 ,2.1 ,2.2 ,2.1 ,2.4, 2.2, 2.8, 2.0],
+        "axis": ["2016","2017","2018","2019","2020","2021","2022","2023","2024","2025"]
         },
         {
         "name": "South East",
+        "bullets": [
+                    "<li>Investment Level: £94</li>",
+                    "<li>Total schemes: 32</li>",
+                    "<li>Major Schemes: 15</li>",
+                    "<li>Minor Schemes: 17</li>"
+                    ],
+        "detail": [
+                    "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>",
+                    "<p>Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. </p> ",
+                    "<p>Vestibulum rhoncus efficitur velit, fermentum dictum sem suscipit quis. Nunc fermentum nisl a condimentum volutpat. Maecenas ultrices tortor et arcu euismod condimentum. </p>"
+                    ],
+        "impact": [
+                    "<li>Sed dignissim lacinia nunc. Curabitur tortor.</li>",
+                    "<li>Pellentesque nibh. Aenean quam. In scelerisque sem at dolor.</li>",
+                    "<li>Maecenas mattis. Sed convallis tristique sem.</li>",
+                    "<li>Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa.</li>",
+                    "<li>Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum.</li>"
+                   ],
+        "investment": [88,12,8,6,4,0,0,0,0,0],
+        "impact-data": [0,1,1,1,5,62,54,50,52,58],
+        "economic": [1.3, 1.4, 2.0, 2.7, 1.9, 1.6 ,1.1 , 1.5, 1.3, 1.7],
+        "environmental":[1.5, 1.1, 1.3, 0.5, 1.2, -1.3, -3.3, -4.1, -2.5, -1.9],
+        "social": [2.1 ,2.4, 2.9 ,2.1 ,2.2 ,2.1 ,2.4, 2.2, 2.8, 2.0],
+        "axis": ["2016","2017","2018","2019","2020","2021","2022","2023","2024","2025"]
         },
         {
         "name": "East of England",
+        "bullets": [
+                    "<li>Investment Level: £103</li>",
+                    "<li>Total schemes: 28</li>",
+                    "<li>Major Schemes: 14</li>",
+                    "<li>Minor Schemes: 14</li>"
+                    ],
+        "detail": [
+                    "<p><Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>",
+                    "<p>Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. </p> ",
+                    "<p>Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.</p>"
+                    ],
+        "impact": [
+                    "<li>Curabitur tortor. Pellentesque nibh. Aenean quam. </li>",
+                    "<li>In scelerisque sem at dolor. Maecenas mattis.</li>",
+                    "<li>Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor.</li>",
+                    "<li>Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. </li>",
+                    "<li>Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum.</li>"
+                   ],
+        "investment": [88,83,68,44,32,12,12,12,6,6],
+        "impact-data": [0,0,0,0,0,98,98,98,98,98],
+        "economic": [-3.2, -1.8, -0.5, -0.5, 1.6, 2.5, 2.8, 3.6, 4.1, 4.5],
+        "environmental":[0.1 ,0.4, 0.9 ,0.1 ,0.2 ,0.1 ,0.4, 0.2, 0.8, 0.1],
+        "social": [-1.5, -1.1, -1.3, -0.5, -1.2, -1.3, -3.3, -4.1, -2.5, -1.9],
+        "axis": ["2016","2017","2018","2019","2020","2021","2022","2023","2024","2025"]
         }
     ]
 };
